219f2cd765dcfc1a63b014a32bdc13a218074142
Hugo Gouzy <110604228+gotin23@users.noreply.github.com>
2023-07-16 17:31:36 +0200

    ajout du router et des routes

--- p11/src/App.js
+++ p11/src/App.js
@@ -1,15 +1,23 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Footer from "./layouts/Footer/Footer";
 import Header from "./layouts/Header/Header";
 import Home from "./pages/Home/Home";
 import SignIn from "./pages/SignIn/SignIn";
+import Users from "./pages/Users/Users";
 
 function App() {
   return (
     <div className="App">
       <Header />
       {/* <SignIn /> */}
-      <Home />
+      {/* <Home /> */}
+      {/* <Users /> */}\
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
       <Footer />
     </div>
   );
--- p11/src/index.js
+++ p11/src/index.js
@@ -2,7 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Header from "./layouts/Header/Header";
+
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
